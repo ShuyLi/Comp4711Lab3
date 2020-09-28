d50d1a9f8f48f505f2de8585fffa890317342f04
Jack_M <Jack_M@LAPTOP-A7R9C1EU>
2020-09-28 21:18:12 +0800

    add an index page

--- index.php
+++ index.php
@@ -17,6 +17,8 @@ $students = array();
 $first = new Student(); $first->surname = "Doe"; $first->first_name = "John"; $first->add_email('home','john@doe.com'); $first->add_email('work','jdoe@mcdonalds.com'); $first->add_grade(65); 
 $first->add_grade(75); $first->add_grade(55); $students['j123'] = $first; 
   $second = new Student(); $second->surname = "Einstein"; $second->first_name = "Albert"; $second->add_email('home','albert@braniacs.com'); $second->add_email('work1','a_einstein@bcit.ca'); $second->add_email('work2','albert@physics.mit.edu'); $second->add_grade(95); $second->add_grade(80); $second->add_grade(50); $students['a456'] = $second; 
+  $third = new Student(); $third->surname = "Li"; $third->first_name = "Shuyu"; $third->add_email('home','shuyuli@doe.com'); $third->add_email('work','cka@qq.com'); $third->add_grade(85); $third->add_grade(70); $third->add_grade(85); $students['a789'] = $third; 
+  ksort($students); 
 foreach($students as $student) 
     echo $student->toString();
 ?> 
